f992d68880c9cd1ae763c67846ef22999dcf7595
mortalbeingg <114620075+mortalbeingg@users.noreply.github.com>
2024-06-02 20:10:26 +0530

    everything updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.1"
 REPO_NAME = "mongodbconnectorpkg"
-PKG_NAME = "databaseautomation"
+PKG_NAME = "mongodbautomation"
 AUTHOR_USER_NAME = "mortalbeingg"
 AUTHOR_EMAIL = "newmortalbeing333@gmail.com"
 
